0114f05a441145e64a146e63b986da34a52b51ca
Brad Alexander <brad.alexander913@gmail.com>
2019-09-17 00:43:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
  end
  
  def execute_transaction
-   if self.valid?
+   if self.valid? && @status == "pending"
      @sender.balance -= @amount
      @receiver.balance += @amount 
      @status = "complete"

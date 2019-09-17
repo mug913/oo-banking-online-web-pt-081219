a8377be71123bbdce479e135686a5829096e6e15
Brad Alexander <brad.alexander913@gmail.com>
2019-09-17 00:30:31 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,22 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name, :valid
+
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(ammount)
+    @balance += ammount 
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}." 
+  end
+  
+  def valid
+    if 
 end

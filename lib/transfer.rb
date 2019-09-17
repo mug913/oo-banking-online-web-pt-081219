5f014c74c2b0a7a4314304145767eb44ce867701
Brad Alexander <brad.alexander913@gmail.com>
2019-09-17 00:52:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,8 +21,9 @@ class Transfer
  
  def execute_transaction
    if @status == "pending"
-     binding.pry
+    # binding.pry
      if @sender.balance < @amount || self.valid? == false
+       binding.pry
        "Transaction rejected. Please check your account balance."
      else
       @sender.balance -= @amount
